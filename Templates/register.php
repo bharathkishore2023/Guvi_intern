310cc7e46383468ad2b4d2876068df0bd1e5aae5
Saravanan raghul <saravananraghul1820@gmail.com>
2021-01-24 21:12:40 +0530

    register page v2

--- Templates/register.php
+++ Templates/register.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Login-page || SaravananVijayamuthu</title>
+    <title>Sign up</title>
     <link rel="shortcut icon" href="" type="image/x-icon">
     <link rel="shortcut icon" href="">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Ubuntu&display=swap" rel="stylesheet">
+    <!-- external css -->
     <style>
-        <?php include '../public/css/style.css'; ?>
         <?php include '../public/css/bootstrap.min.css'; ?>
+        <?php include '../public/css/style.css'; ?>
     </style>
 </head>
 
@@ -23,44 +23,49 @@
                     <div class="col-lg-5 col-md-6 no-padding">
                         <form action="" method="POST">
                             <div class="login-box">
-                                <h5><span class="text_1">Welcome Back🖤</span></h5>
+                                <h5><span class="text_1">Welcome🖤</span></h5>
                                 <div class="login-row row no-margin">
-                                    <label for=""> Email Address</label>
-                                    <input type="email" name="email" class="form-control form-control-sm" required>
+                                    <label for="Full Name">Full Name</label>
+                                    <input type="fname" name="fname" class="form-control form-control-sm"  autocomplete="name" required>
                                 </div>
                                 <div class="login-row row no-margin">
-                                    <label for=""> Password</label>
-                                    <input type="password" name="password" class="form-control form-control-sm" required>
+                                    <label for="Email">Email Address</label>
+                                    <input type="email" name="email" class="form-control form-control-sm"  autocomplete="username" required>
                                 </div>
-                                <div class="login-row row forrr no-margin">
-                                    <p> <input type="checkbox"> Remember Me</p>
-                                    <p><a class="vgh" href="">Forget Password?</a></p>
+                                <div class="login-row row no-margin">
+                                    <label for="Password">Password</label>
+                                    <input type="password" name="password" class="form-control form-control-sm" autocomplete="new-password" required>
+                                </div>
+                                <div class="login-row row no-margin">
+                                    <label for="Confirm Password">Confirm Password</label>
+                                    <input type="password" name="cpassword" class="form-control form-control-sm" autocomplete="new-password" required>
                                 </div>
                                 <div class="login-row btnroo row no-margin">
                                     <button class="btn btn-primary btn-sm" id="submit" value="Send"> Sign In</button>
                                     <button class="btn btn-success btn-sm"> Create Account</button>
                                 </div>
                                 <div class="login-row donroo row no-margin">
-                                    <p>Dont have an Account ? <a href="">Sign Up</a></p>
+                                    <p>Already have an account?<a href="">&nbsp;Sign in</a></p>
                                 </div>
                             </div>
                         </form>
                     </div>
                     <div class="col-lg-7 col-md-6 img-box">
-                        <img src="assets/images/login.gif" alt="">
+                        <img src="../public/images/signup.svg" alt="">
                     </div>
                 </div>
                 <div class="card  alert-info align-baseline">
                     <div class="card-body">
-                        <p>By: <a href="https://sarvi.azurewebsites.net/">Saravanan Vijayamuthu</a></p>
+                        <p>By: <a href="https://saravananvijayamuthu.herokuapp.com/">Saravanan Vijayamuthu</a></p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </body>
+<!-- external js -->
 <script>
-<?php include '../public/js/bootstrap.min.js'; ?>
 <?php include '../public/js/popper.min.js'; ?>
+<?php include '../public/js/bootstrap.min.js'; ?>
 </script>
 </html>
