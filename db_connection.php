772b1a35ca3f2544b6597bd927e4373f0abafe62
Saravanan raghul <saravananraghul1820@gmail.com>
2021-01-26 23:43:57 +0530

    try4

--- db_connection.php
+++ db_connection.php
@@ -1,9 +1,9 @@
 <?php
 /* Database connection start */
 $servername = "127.0.0.1";
-$username = "root";
+$username = "";
 $password = "";
-$dbname = "guvi";
+$dbname = "";
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
