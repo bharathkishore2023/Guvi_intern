ef6c555eed9d7fa1c3c6f1d00fd4ed2b140fc7e6
Saravanan raghul <saravananraghul1820@gmail.com>
2021-01-29 19:53:16 +0530

    db psw removed

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "127.0.0.1";
-$username = "root";
-$password = "sarvi";
-$dbname = "guvi";
+$servername = "localhost";
+$username = "";
+$password = "";
+$dbname = "";
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
