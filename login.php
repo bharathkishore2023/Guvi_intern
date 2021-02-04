c1e53c69c3bfe45e30f59eb9c13266cc15df2468
Saravanan raghul <saravananraghul1820@gmail.com>
2021-02-05 00:19:09 +0530

    LocalStorage Login and Displayed email in Loggedin

--- login.php
+++ login.php
@@ -70,12 +70,12 @@
                         cache: false,
                         success: function(response) {
                             if (response == "LoginSuccess") {
-                                $("#submit").html(' Processing ...');
+                                var lEmail = localStorage.setItem('email', user_email);
                                 window.location.href = "loggedin.php";
                             } else if (response == "Fail") {
                                 $("#error").fadeIn(1000, function() {
+                                    localStorage.clear();
                                     $("#error").html('<div class="alert alert-danger" style="text-align: center"> Check your mail and password</div>');
-                                    $("#submit").html('Create Account');
                                 });
                             }
                         }
