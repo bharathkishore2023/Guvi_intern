be7491b65d86f4df367328280a2e7ad67a1fde2e
Saravanan raghul <saravananraghul1820@gmail.com>
2021-02-05 10:21:19 +0530

    Local Storage welcome message

--- loggedin.php
+++ loggedin.php
@@ -45,13 +45,11 @@ session_start();
                                     <form id="register-form" class="form-signin" method="POST">
                                         <div class="login-box">
                                             <h5><span class="text_1_login">Welcome Back <img src="./public/images/logo.svg" style="width: 20px;"></span></h5>
-                                            <div class="card-columns">
-                                                <div class="card bg-success text-white">
-                                                    <div class="card-body text-center">
+                                                <div class="card border-success mb-3"  style="max-width: 20rem;">
+                                                    <div class="card-body text-center font-weight-bold">
                                                         <p class="card-text" id="Welcome"></p>
                                                     </div>
                                                 </div>
-                                            </div>
                                                 <div class="login-row row no-margin">
                                                     <label for="username">User Name</label>
                                                     <input type="text" name="user_name" id="user_name" class="form-control form-control-sm" value='<?php echo $row['user_name'] ?>'>
@@ -65,7 +63,7 @@ session_start();
                                                     <input type="text" name="user_linkedin" id="user_linkedin" class="form-control form-control-sm" value='<?php echo $row['user_linkedin'] ?>'>
                                                 </div>
                                                 <div class="login-row btnroo row no-margin">
-                                                    <button class="btn btn-primary btn-sm" name="create_acc" id="btn-submit" value="submit">Update</button>
+                                                    <button class="btn btn-primary btn-sm" name="create_acc" id="btn-submit" value="submit">Update / View profile</button>
                                                 </div>
                                                 <div class="login-row donroo row no-margin">
                                                     <a href="logout.php">Logout</a>
@@ -96,7 +94,7 @@ session_start();
 </body>
 <script>
     if (localStorage) {
-        document.getElementById('Welcome').innerHTML += localStorage.getItem('email');
+        document.getElementById('Welcome').innerHTML +=" Logged in as "+ localStorage.getItem('email');
     } else {
         alert("Sorry, your browser do not support local storage.");
     }
